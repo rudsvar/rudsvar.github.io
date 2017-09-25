41473ce4520e5d43e17fb6e651973e2bcfb81d18
Rudi B. Svartveit <rudi.svartveit@student.uib.no>
2017-09-25 20:26:24 +0200

    Didn't work :(

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
 <html>
 	<title> Rudi's Cool Page </title>
 	<head> Hello, am I cool yet? <br> </head>
-
-	<body>
-	How to count? <br>
-	<?php
-	
-	for ($i = 0; $i < 10; i++) {
-		echo "Number is $i. <br>";
-	}
-	
-	?>
-	
-	</body>
 </html>
