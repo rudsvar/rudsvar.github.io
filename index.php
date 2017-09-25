9bf3a635ca9da8a107d9cba8b710fb2ac18167d3
Rudi B. Svartveit <rudi.svartveit@student.uib.no>
2017-09-25 20:24:22 +0200

    Add counting

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+	<title> Rudi's Cool Page </title>
+	<head> Hello, am I cool yet? <br> </head>
+
+	<body>
+	How to count? <br>
+	<?php
+	
+	for ($i = 0; $i < 10; i++) {
+		echo "Number is $i. <br>";
+	}
+	
+	?>
+	
+	</body>
+</html>
